a8613b939d38236533d316191659ef73190828dc
anggialansori404 <anggialansori.rpl4@gmail.com>
2021-08-23 02:17:09 +0000

    mojito: Nusantara-ify
    Signed-off-by: anggialansori404 <anggialansori.rpl4@gmail.com>

--- nad_mojito.mk
+++ nad_mojito.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The PixelExperience Project
+# Copyright (C) 2021 NusantaraProject
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,14 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some common PixelExperience stuff
+# Inherit some common Nusantara stuff
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+USE_PIXEL_CHARGING := true
+USE_GAPPS ?= true
+TARGET_USES_BLUR := true
+NAD_BUILD_TYPE := OFFICIAL
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_mojito
+PRODUCT_NAME := nad_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
